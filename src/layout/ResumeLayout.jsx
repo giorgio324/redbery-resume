3671c187dc0598ef9aef8cc445ac172b7f4edf6b
giorgi <giorgikochuashvili2002@gmail.com>
2023-05-13 05:33:49 +0400

    fiexed responsiveness swapped grid for flex

--- src/layout/ResumeLayout.jsx
+++ src/layout/ResumeLayout.jsx
@@ -4,12 +4,17 @@ import Preview from '../components/Preview';
 
 const ResumeLayout = ({ children }) => {
   return (
-    <main className='min-h-screen bg-grey flex'>
+    <main className='min-h-screen bg-grey grid grid-cols-[auto,auto] overflow-x-auto'>
       <section className='flex flex-grow p-[45px] gap-x-[60px]'>
         <BackButton />
         <div className='flex-grow'>{children}</div>
       </section>
-      <div className='flex-grow'>
+      {/* 
+            alternative styles check it later
+          <div className='max-h-screen overflow-y-auto'>
+            <Preview />
+          </div> */}
+      <div className=''>
         <Preview />
       </div>
     </main>
