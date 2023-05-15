bec5c0fc081899dd4220eee62aeab94f0bfd380d
giorgi <giorgikochuashvili2002@gmail.com>
2023-05-15 20:42:52 +0400

    fixed padding problem on private info

--- src/components/PreviewContainer.jsx
+++ src/components/PreviewContainer.jsx
@@ -2,7 +2,7 @@ import PrivateInfoPreview from './PrivateInfoPreview';
 import watermark from '../assets/images/previewWatermark.svg';
 const PreviewContainer = () => {
   return (
-    <section className='relative min-h-screen bg-white min-w-[820px] max-w-[820px] px-[80px] pt-[50px] pb-[144px]'>
+    <section className='relative min-h-full bg-white min-w-[820px] max-w-[820px] px-[80px] pt-[50px] pb-[144px]'>
       <PrivateInfoPreview />
       <img
         src={watermark}
--- src/layout/ResumeLayout.jsx
+++ src/layout/ResumeLayout.jsx
@@ -5,7 +5,7 @@ import PreviewContainer from '../components/PreviewContainer';
 const ResumeLayout = ({ children }) => {
   return (
     <main className='min-h-screen bg-grey grid grid-cols-[1fr,auto] overflow-x-auto'>
-      <section className='flex flex-grow p-[45px] gap-x-[60px]'>
+      <section className='flex flex-grow py-[45px] pl-[50px] pr-[150px]  gap-x-[60px]'>
         <BackButton />
         <div className='flex-grow w-full'>{children}</div>
       </section>
