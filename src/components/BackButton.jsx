d2f0df1853065f0315a76025f612f8bda6a62582
giorgi <giorgikochuashvili2002@gmail.com>
2023-05-25 05:53:06 +0400

    added dispatch to clear first page slice

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,8 +1,15 @@
 import { Link } from 'react-router-dom';
 import backArrow from '../assets/images/backBtnArrow.svg';
+import {
+  clearPrivateInfo,
+  updatePrivateInfo,
+} from '../features/PrivateInfoSlice';
+import { useDispatch } from 'react-redux';
 const BackButton = () => {
+  const dispatch = useDispatch();
   const handleLocalStorageClear = () => {
     localStorage.clear();
+    dispatch(clearPrivateInfo());
   };
 
   return (
